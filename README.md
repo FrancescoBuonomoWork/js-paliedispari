d5a1040ce3b1239aaa31e6506db06fdbb61321a5
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-12 15:22:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Palindroma
+## Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+- Chiedere all’utente di inserire una parola
+    - creare il prompt e immagazzinare la parola in una variabile
+- creare una funzione che verifichi se la parola è palindroma
+    - stabilire il nome della funzione (isPalindroma)
+    - SE è palindroma dare come risultato TRUE
+        - 
+    - altrimenti dare come risultato FALSE
+- verificare che la parola inserita dall'utente sia palindroma tramite la funzione    
---
+++ js/palindroma.js
@@ -0,0 +1,37 @@
+// console.log("palidroma");
+
+const userWord = prompt("Inserisci una parola per verificare che sia palindroma");
+console.log(userWord);
+console.log(userWord.length);
+
+
+
+// const fromWordToArray = userWord.split("");
+// console.log(fromWordToArray);
+// const ArrayReverse = fromWordToArray.reverse();
+// console.log(ArrayReverse);
+
+function isPalindrome(word) {
+    const fromWordToArray = word.split("");
+    console.log(fromWordToArray);
+    const arrayReverse = fromWordToArray.reverse();
+    console.log(arrayReverse);
+    const wordReverse = arrayReverse.join("");
+    console.log(wordReverse);
+
+
+
+    if (word === wordReverse) {
+       return true
+        
+    } else {
+       return false
+    }
+}
+
+if (isPalindrome(userWord)){
+    console.log("la parola è palindroma")
+}else {
+    console.log("la parola non è palindroma");
+}
+// console.log(userWord);
